4cf088603878a8b77ddc62aad6051a93a12ef2c4
LunaRenilson <renilson.c.luna@gmail.com>
2025-04-08 07:06:51 -0300

    trabalho1 finalizado

--- trabalho1/atv1.1.py
+++ trabalho1/atv1.1.py
@@ -3,13 +3,12 @@ import numpy as np
 import os
 
 # imagem = cv.imread('imagem.jpg')
-imagemCinza= cv.imread(f'imgs/perfil.jpeg', cv.IMREAD_GRAYSCALE)
+imagemCinza= cv.imread(f'imgs/relogio.png', cv.IMREAD_GRAYSCALE)
 
 redimensionado = cv.resize(imagemCinza, (0, 0), fx = 0.5, fy = 0.5)
 
 # Suavizando imagem com gaussianBlue
 imagemSuavizado = cv.GaussianBlur(redimensionado, (31, 31), 0)
-
 # Constante para evitar divisão por 0 
 constante = 1
 imagemLapis = cv.divide(redimensionado, imagemSuavizado + constante, scale=256)
@@ -17,4 +16,4 @@ imagemLapis = cv.divide(redimensionado, imagemSuavizado + constante, scale=256)
 concatenado = cv.hconcat([redimensionado, imagemLapis])
 
 # Salvando imagem
-cv.imwrite('./imgs_geradas/atv1.1/source.jpeg', concatenado)
+cv.imwrite('./imgs_geradas/atv1.1/source.png', concatenado)
